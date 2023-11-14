6d8516436c4d135b2de61caa2b797de95d7fd221
yuliialiubenka <77937285+yuliialiubenka@users.noreply.github.com>
2023-11-14 19:21:07 +0000

    Deploying to gh-pages from @ yuliialiubenka/portfolio@3e165addd9210b7e7217230f249fd3bede7294e6 🚀

---
+++ index.e95298e1.js
@@ -0,0 +1,2 @@
+window.onload=function(){document.body.classList.add("loaded_hiding"),window.setTimeout((function(){document.body.classList.add("loaded"),document.body.classList.remove("loaded_hiding")}),500)};
+//# sourceMappingURL=index.e95298e1.js.map
